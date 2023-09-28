bb3f356e77273ed10b86edcf6245757f3105b09f
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2023-09-28 16:05:34 +0530

    add redux persist

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -69,7 +69,7 @@ function SignIn() {
       <div className="flex gap-2 mt-5">
         <p>Dont have an account?</p>
         <Link to={"/sign-up"}>
-          <span className="text-blue-700">Sign in</span>
+          <span className="text-blue-700">Sign Up</span>
         </Link>
       </div>
       {error && <p className="text-red-500 mt-5">{error}</p>}
